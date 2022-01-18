39c23932ca0f835ed79457bf3d83b9064703e11b
Loreen <loreenmoraa@gmail.com>
2022-01-18 22:48:52 +0300

    declare a variable for male and female names

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 function getAkanName() {
 
-  
+  var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+  var femaleNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+
 }
