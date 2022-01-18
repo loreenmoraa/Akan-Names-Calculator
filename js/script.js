4f5bcc94636d5a07b6aac6f2069f9d2514f3bbb4
Loreen <loreenmoraa@gmail.com>
2022-01-18 22:50:11 +0300

    declare dd mm yy variables

--- js/script.js
+++ js/script.js
@@ -3,4 +3,13 @@ function getAkanName() {
   var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
   var femaleNames = ["Akosua", " Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
+  //Variables declaration
+  var day = document.getElementById("day").value;
+  var dd = parseInt(day);
+  var month = document.getElementById("month").value;
+  var mm = parseInt(month);
+  var year = document.getElementById("year").value;
+  var yy = parseInt(year.toString().slice(2, 4));
+  var cc = Math.ceil(year / 100)
+
 }
